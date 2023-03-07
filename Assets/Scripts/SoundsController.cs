8705473d0e3f3f0a3c5400db6f725a7c67448a3c
frozway3 <perelexa2.0@gmail.com>
2023-03-07 10:42:41 +0300

    grafix and sounds
    Надо будет настроить музыку

---
+++ Assets/Scripts/SoundsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SoundsController : MonoBehaviour
+{
+    [SerializeField] AudioSource onClickAudio, BGAudio;
+    [SerializeField] AudioClip[] BGMusic;
+    private void Start()
+    {
+        StartCoroutine(PlayBGMusic());
+    }
+    public void Click(AudioClip clip)
+    {
+        onClickAudio.PlayOneShot(clip);
+    }
+    public void RandomMusic()
+    {
+        BGAudio.clip = BGMusic[Random.Range(0, BGMusic.Length)];
+    }
+    IEnumerator PlayBGMusic()
+    {
+        while (true)
+        {
+            BGAudio.Stop();
+            RandomMusic();
+            BGAudio.Play();
+            yield return new WaitForSeconds(24);
+        }
+    }
+}
